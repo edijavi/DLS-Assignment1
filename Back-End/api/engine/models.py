c094ab776c58b49ff6c396b10ffa164375d23ae3
Sanperolo <michaelherranz@gmail.com>
2019-03-07 16:36:25 +0100

    Added Models database

--- Back-End/api/engine/models.py
+++ Back-End/api/engine/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class File(models.Model):
+    path = models.CharField(max_length = 500)
+    name = models.CharField(max_length = 50)
+    nchars = models.IntegerField()
+    nlines = models.IntegerField()
+
+class Word(models.Model):
+    value = models.CharField(max_length = 500)
+    file = models.ForeignKey(File, on_delete=models.CASCADE)
+    line = models.IntegerField()
