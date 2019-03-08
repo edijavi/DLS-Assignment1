077f7bd8df209ee0fe543594be6b845a3de0c8fd
rgarcia <rgarcia@transparentcdn.com>
2019-03-08 18:26:39 +0100

    - Improved ETL Command and given functionality.

--- Back-End/api/engine/management/commands/etl_engine.py
+++ Back-End/api/engine/management/commands/etl_engine.py
@@ -1,4 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from django.conf import settings
+
 from django.core.management.base import BaseCommand, CommandError
+from engine.models import File, Word
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+FOLDER = 'Data'
+PATH = os.path.join(settings.BASE_DIR, FOLDER)
+
 
 class Command(BaseCommand):
     help = '''                 ETL 
@@ -17,4 +29,31 @@ class Command(BaseCommand):
         # parser.add_argument('poll_id', nargs='+', type=int)
 
     def handle(self, *args, **options):
-        print('TODO')
+        # r=root, d=directories, f = files
+        cont = 0
+        for r, d, f in os.walk(PATH):
+            for file in f:
+                file_path = self.get_file_path(r, file)
+                if cont == 0:
+                    self.open_file(file_path)
+                    cont += cont + 1
+    def get_file_path(self, root, file):
+
+        return os.path.join(root, file)
+
+    def open_file(self, path):
+        lines=0
+        words=0
+        total_words=0
+        characters=0
+        with open(path, 'r') as all_text:
+            for l in all_text:
+                words=l.split()
+                lines+=1
+                total_words+=len(words)
+                for i in words:
+                    characters+=len(i)
+        print('Total lines: {}'.format(lines))
+        print('Total Words: {}'.format(total_words))
+        print('Total Characters: {}'.format(characters))
+                
