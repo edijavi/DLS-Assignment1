2e7e504e123642300bd2a4ecb06507fbc7fedede
rgarcia <rgarcia@transparentcdn.com>
2019-03-10 02:50:39 +0100

    Finished ETL Command and charged all the raw data into sqlite DB. Introduced pagination in the API

--- Back-End/api/engine/management/commands/etl_engine.py
+++ Back-End/api/engine/management/commands/etl_engine.py
@@ -24,36 +24,61 @@ class Command(BaseCommand):
             3. Transform/Clean all the words and insert into the DB!
            '''
 
-    def add_arguments(self, parser):
-        pass
-        # parser.add_argument('poll_id', nargs='+', type=int)
-
     def handle(self, *args, **options):
         # r=root, d=directories, f = files
-        cont = 0
         for r, d, f in os.walk(PATH):
             for file in f:
                 file_path = self.get_file_path(r, file)
-                if cont == 0:
-                    self.open_file(file_path)
-                    cont += cont + 1
-    def get_file_path(self, root, file):
+                self.open_file(file_path)
 
+    def get_file_path(self, root, file):
         return os.path.join(root, file)
 
+    def get_file_name(self, path):
+        return os.path.basename(path)
+
     def open_file(self, path):
+        # File properties
         lines=0
         words=0
         total_words=0
         characters=0
+
+        # Word Properties
+        words_to_insert = []
+
         with open(path, 'r') as all_text:
             for l in all_text:
+                # Get file props.
                 words=l.split()
                 lines+=1
                 total_words+=len(words)
+
+                
                 for i in words:
+                    # Get Words in-line
+                    words_to_insert.append((i, lines))
+                    # Get all characters per word
                     characters+=len(i)
+
+        print('Loading...\n')
+        _file = self.perform_insert_file(path, self.get_file_name(path), characters, lines)
+        self.perform_insert_words(_file, words_to_insert)
+
+        print('---- File: {} ---- \n'.format(self.get_file_name(path)))
         print('Total lines: {}'.format(lines))
         print('Total Words: {}'.format(total_words))
         print('Total Characters: {}'.format(characters))
+        print('--------------------------------- \n')
                 
+                
+    def perform_insert_file(self, path, name, characters, lines):
+        return File.objects.create(path=path, name=name, nchars=characters, nlines=lines)
+
+    def perform_insert_words(self, file, words):
+        entity_words = [Word(value=word[0], line=word[1], file=file) for word in words]
+        # Bulk Creation in order to improve the insert perform
+        # https://docs.djangoproject.com/en/2.0/ref/models/querysets/#django.db.models.query.QuerySet.bulk_create
+
+        Word.objects.bulk_create(entity_words)
+            
